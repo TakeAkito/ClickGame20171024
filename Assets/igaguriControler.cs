ec94ce9f3126335a27db4a7ba31338396ac8a2ec
Akito Takeuchi <nkc.akito.takeuchi@gmail.com>
2017-10-26 11:13:49 +0900

    地面を設置して弾を飛ばす

---
+++ Assets/igaguriControler.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class igaguriControler : MonoBehaviour {
+
+    // Use this for initialization
+    public void Shoot(Vector3 dir)
+    {
+        GetComponent<Rigidbody>().AddForce(dir);
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        GetComponent<Rigidbody>();
+    }
+	void Start ()
+    {
+        //Shoot(new Vector3(0, 200, 2000));
+	}
+}
---
+++ Assets/igaguriGenerator.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class igaguriGenerator : MonoBehaviour{
+
+public GameObject igaguriPrefab;
+
+	void Update ()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject igaguri = Instantiate(igaguriPrefab) as GameObject;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 worldDir = ray.direction;
+            igaguri.GetComponent<igaguriControler>().Shoot(worldDir.normalized * 2000);
+        }
+	}
+}
